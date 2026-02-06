d7b9828503081331fb4299813350fa5bc85268b4
stort040 <stort040@umn.edu>
2026-02-05 19:10:01 -0600

    Add Main Menu and Credits

---
+++ Godot/Levels/testing.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	# exit to main menu when ESC is pressed
+	if Input.is_action_just_pressed("ui_cancel"): 
+		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
