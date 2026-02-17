346fab460596ea8145b712d1a074a794b4ea70c7
buildNacho-school <curti750@umn.edu>
2026-02-16 20:12:30 -0600

    Rotating platform Start
    Began working on rotating platform. Technically works but it needs smore work

---
+++ Godot/Modular Level Stuff/rotating_platform.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+var torque := 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	rotate(torque * delta)
+	print("total", torque * delta)
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("PLAYER"):
+		var radius = -(body.position - position).length()
+		print("right:", (radius * (body.velocity).length()) * 0.000007)
+		torque += (radius * (body.velocity).length()) * 0.000007
+		
+
+
+func _on_left_body_entered(body: Node2D) -> void:
+	if body.is_in_group("PLAYER"):
+		var radius = (body.position - position).length()
+		print("LEFT:", (radius * (body.prev_velocity).length()) * 0.000007)
+		torque += (radius * (body.prev_velocity).length()) * 0.000007
