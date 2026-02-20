58b7eba0207fc4d23605f5c7f449f76c05abce6a
buildNacho-school <curti750@umn.edu>
2026-02-19 19:38:23 -0600

    Fixed rotation platform
    Now with damping and rotaitonal miultipliers!

--- Godot/Scripts/rotating_platform.gd
+++ Godot/Scripts/rotating_platform.gd
@@ -1,8 +1,10 @@
 extends StaticBody2D
 
 var torque := 0.0
+@export var damping := 0.0
+@export var rotation_mult := 1.0
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.S
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -10,17 +12,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	rotate(torque * delta)
+	rotation_degrees = clampf(rotation_degrees, -35, 35)
+	if rotation_degrees == 35 or rotation_degrees == -35:
+		torque = 0
+		
+	print(torque)
+	torque = move_toward(torque, 0, damping * delta)
 	# print("total", torque * delta)
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		var radius = -(body.position - position).length()
-		print("right:", (radius * (body.velocity).length()) * 0.000007)
-		torque += (radius * (body.velocity).length()) * 0.000007
+		
+		torque += (radius * (body.velocity).length()) * (0.000007 * rotation_mult)
 		
 
 
 func _on_left_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		var radius = (body.position - position).length()
-		print("LEFT:", (radius * (body.prev_velocity).length()) * 0.000007)
-		torque += (radius * (body.prev_velocity).length()) * 0.000007
+		
+		torque += (radius * (body.prev_velocity).length()) * (0.000007 * rotation_mult)
