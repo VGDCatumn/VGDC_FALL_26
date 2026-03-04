655a72b0b54b5f397b8485ef7e283b3dc0a2ac33
leotpham3856 <leotpham202@gmail.com>
2026-03-04 10:13:50 -0600

    NPC node finish

--- Godot/Instances/npc.gd
+++ Godot/Instances/npc.gd
@@ -1,17 +1,37 @@
 extends Node2D
 
-var is_chatting = false
-
 var player
 var player_in_speakZone = false
+var done_talking = true
 
-func _on_speakZone_entered(body):
-	if body.has_method("player"):
+
+func _process(delta):
+	if (player_in_speakZone == true) and (done_talking == true):
+		done_talking = false
+		$Dialogue.start()	
+		$Dialogue/Timer.start()
+		print("HERE")
+	
+
+
+func _on_dialogue_dialogue_finished():
+	$Dialogue/Timer.paused
+
+
+	
+
+
+func _on_speak_zone_body_entered(body):
+	if body.is_in_group("PLAYER"):
 		player = body
 		player_in_speakZone = true
+		print("in zone")
 		
-func _on_speakZone_exited(body):
-	if body.has_method("player"):
+		
+
+
+func _on_speak_zone_body_exited(body):
+	if body.is_in_group("PLAYER"):
 		player_in_speakZone = false
+		done_talking = true
 		
-	
