3441691cd18d537651dc080b5f3a9ca39db3f0ca
eclipti-c <cosmicfire2988@gmail.com>
2026-03-26 20:00:36 -0500

    Village level update

--- Godot/UI/UI_Scripts/main_menu.gd
+++ Godot/UI/UI_Scripts/main_menu.gd
@@ -12,7 +12,8 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/level_chasm_temp.tscn")
+	get_tree().change_scene_to_file("res://Levels/level_village.tscn")
+#"res://Levels/level_chasm_temp.tscn"
 
 
 func _on_credits_button_pressed() -> void:
