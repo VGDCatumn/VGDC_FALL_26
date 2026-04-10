df9fd874a6440b5da4e6565eee9161abd1eee9c0
eclipti-c <cosmicfire2988@gmail.com>
2026-04-09 19:57:30 -0500

    Village Second Iteration

--- Godot/Scripts/platform_trampoline.gd
+++ Godot/Scripts/platform_trampoline.gd
@@ -1,18 +1,34 @@
 extends Area2D
 
-@export var spring_power := 1.4
-@export var bounce_threshold := 2000
+@export var trampoline_power := 1.4
+@export var bounce_limit := 2500
+@export var clamp_timer := 3  #timer in secs before the velocity clamp is reapplied
+
+var	prev_max_velocity	#stores current max_y_velocity set in ball_man
+var player_ref: Node2D 
+
+func _ready() -> void:
+	$Timer.wait_time = clamp_timer
+	$Timer.one_shot = true 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
+		player_ref = body
 		
-		# gets velocity from play scirpt
+		prev_max_velocity = body.max_y_velocity #stores Player's current max y velocity 
+
+		# gets velocity from player scirpt
 		var prev_vel = body.prev_velocity.y
 		
 		# get launch_direction by taking the UP vector of the spring
 		var launch_direction = -transform.y
-		body.velocity = launch_direction * (-prev_vel * spring_power)
+		body.max_y_velocity = bounce_limit
+		body.velocity = launch_direction * (-prev_vel * trampoline_power)
 		
 		# give player more air control
 		body.has_aerial_movement = true
-	
+		$Timer.start()
+
+#Once the timer runs out reapplies max y velocity (jump height) to player
+func _on_timer_timeout() -> void:
+	player_ref.max_y_velocity = prev_max_velocity
