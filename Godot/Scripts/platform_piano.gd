0d8379b04c089ffd6ce7de564298bd6092c35a8f
MagmaC4 <magmac495@gmail.com>
2026-04-15 16:55:22 -0500

    Add piano note sound on collision

--- Godot/Scripts/platform_piano.gd
+++ Godot/Scripts/platform_piano.gd
@@ -2,11 +2,5 @@ extends Node2D
 
 @onready var audio : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_piano_keys_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	audio.play()
