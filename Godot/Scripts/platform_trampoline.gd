8cfe859e61d8b6891e7a56cfc4b581dbe4b8f923
MagmaC4 <magmac495@gmail.com>
2026-04-16 02:12:06 -0500

    Significantly change level_village, refactor trampoline script

--- Godot/Scripts/platform_trampoline.gd
+++ Godot/Scripts/platform_trampoline.gd
@@ -1,15 +1,12 @@
 extends StaticBody2D
 
-@export var trampoline_power := 1.4
-@export var bounce_limit := 4000 #max height you can gain from bouncing off trampoline
+@export var trampoline_power := 1.2
+@export var bounce_limit := 3000 #max height you can gain from bouncing off trampoline
 @export var clamp_timer := 3  #timer in secs before the velocity clamp is reapplied
 
-var	prev_max_velocity	#stores current max_y_velocity set in ball_man
+# Placeholder values, determined on runtime
+var	prev_max_velocity	
 var player_ref: Node2D
-
-@onready var area = $Detecter
-
- 
 
 func _ready() -> void:
 	$Timer.wait_time = clamp_timer
@@ -17,23 +14,22 @@ func _ready() -> void:
 
 func _on_detecter_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
-		player_ref = body
-		
-		prev_max_velocity = body.max_y_velocity #stores Player's current max y velocity 
-
-		# gets velocity from player scirpt
-		var prev_vel = body.prev_velocity.y
-		
-		# get launch_direction by taking the UP vector of the spring
-		var launch_direction = -transform.y
-		
+		# change player max_y_velocity on trampoline bounce, reset on timer
+		prev_max_velocity = body.max_y_velocity 
 		body.max_y_velocity = bounce_limit
-		body.velocity = launch_direction * ((-prev_vel  * 0.8) * trampoline_power)
+		player_ref = body # player reference for timer to adjust max_y_velocity
+		$Timer.start()
+
+		# bounce player on trampoline
+		var launch_direction = -transform.y # up direction
+		var prev_vel = body.prev_velocity.y # player velocity
+		body.velocity.y = 0
+		body.velocity += launch_direction * (-prev_vel * trampoline_power)
+		
 		
 		# give player more air control
-		body.has_aerial_movement = true
+		# body.has_aerial_movement = true
 		
-		$Timer.start()
 		$AnimationPlayer.play("trampoline_bounce")
 
 #Once the timer runs out reapplies max y velocity (jump height) to player
