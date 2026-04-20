a3725ba8c53b159705858d42aedbcec04d033f88
MagmaC4 <magmac495@gmail.com>
2026-04-20 18:09:33 -0500

    Add planet assets, add ufo w/ tractor beam

--- Godot/Scripts/level_moon_v_2.gd
+++ Godot/Scripts/level_moon_v_2.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+
+@export var tractor_beam : Node2D
+var player : CharacterBody2D
+var in_tractor_beam : bool = false
+
+func _process(delta):
+	if in_tractor_beam:
+		var distance_vector = tractor_beam.global_position - player.global_position
+		var direction = distance_vector.normalized()
+		var distance = distance_vector.length()
+		player.velocity += direction * ((distance * 1.0/16.0 * delta) ** 5)
+
 # MOON GRAVITY
 func _on_low_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
@@ -25,9 +37,12 @@ func _on_regular_gravity_body_exited(body: Node2D) -> void:
 # UFO TRACTOR BEAM
 func _on_reverse_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
-		body.gravity *= -1
+		body.gravity *= -1.0 / 2.0
+		player = body
+		in_tractor_beam = true
 
 
 func _on_reverse_gravity_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
-		body.gravity *= -1
+		body.gravity *= -2.0
+		in_tractor_beam = false
