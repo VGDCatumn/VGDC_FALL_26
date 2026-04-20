0290d839a1cd7958e198bf13ec6363353c4929b6
MagmaC4 <magmac495@gmail.com>
2026-04-19 22:50:20 -0500

    Add ufo to moon

--- Godot/Scripts/level_moon_v_2.gd
+++ Godot/Scripts/level_moon_v_2.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+# MOON GRAVITY
 func _on_low_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 1.0 / 2.0
@@ -11,6 +11,7 @@ func _on_low_gravity_body_exited(body: Node2D) -> void:
 		body.gravity *= 2.0
 
 
+# EARTH GRAVITY / GRAVITY WELL
 func _on_regular_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 2.0
@@ -19,3 +20,14 @@ func _on_regular_gravity_body_entered(body: Node2D) -> void:
 func _on_regular_gravity_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 1.0 / 2.0
+
+
+# UFO TRACTOR BEAM
+func _on_reverse_gravity_body_entered(body: Node2D) -> void:
+	if body.is_in_group("PLAYER"):
+		body.gravity *= -1
+
+
+func _on_reverse_gravity_body_exited(body: Node2D) -> void:
+	if body.is_in_group("PLAYER"):
+		body.gravity *= -1
