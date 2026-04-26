50893e909b70201aefcff58fa90bb75d66f21615
MagmaC4 <magmac495@gmail.com>
2026-04-26 17:26:17 -0500

    Add higher resolution saturn to level moon

--- Godot/Scripts/level_moon_v_2.gd
+++ Godot/Scripts/level_moon_v_2.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @onready var audio : AudioStreamPlayer2D = $UFO/AudioStreamPlayer2D
 @export var tractor_beam : Node2D
-var player : CharacterBody2D
 var in_tractor_beam : bool = false
+
+var player : CharacterBody2D
+var previous_gravity : float
 
 func _process(delta):
 	if in_tractor_beam:
@@ -13,7 +15,8 @@ func _process(delta):
 		var distance_vector = tractor_beam.global_position - player.global_position
 		var direction = distance_vector.normalized()
 		var distance = distance_vector.length()
-		player.velocity += direction * ((distance * 1.0/16.0 * delta) ** 5)
+		player.velocity += direction * 12
+		player.velocity.y = lerpf(player.velocity.y, -500.0, delta)
 		
 		# play end credits song
 		if (!audio.playing):
@@ -24,17 +27,16 @@ func _on_low_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 1.0 / 2.0
 
-
 func _on_low_gravity_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 2.0
 
 
 # EARTH GRAVITY / GRAVITY WELL
+# This is not used in the level
 func _on_regular_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		body.gravity *= 2.0
-
 
 func _on_regular_gravity_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
@@ -44,12 +46,13 @@ func _on_regular_gravity_body_exited(body: Node2D) -> void:
 # UFO TRACTOR BEAM
 func _on_reverse_gravity_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
-		body.gravity *= -1.0 / 2.0
+		previous_gravity = body.gravity
 		player = body
 		in_tractor_beam = true
+		body.gravity = 0
 
 
 func _on_reverse_gravity_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PLAYER"):
-		body.gravity *= -2.0
+		body.gravity = previous_gravity
 		in_tractor_beam = false
