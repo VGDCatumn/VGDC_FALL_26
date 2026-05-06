150f991915430266a8a10953e7fdc9e3f92b60ad
MagmaC4 <magmac495@gmail.com>
2026-05-06 14:11:46 -0500

    Add end credits

--- Godot/Scripts/credits_scroll.gd
+++ Godot/Scripts/credits_scroll.gd
@@ -1,8 +1,15 @@
 extends RichTextLabel
 
-@export var scroll_speed: float = 50.0
+@export var scroll_speed: float = 40.0
+@onready var scrollbar = get_v_scroll_bar()
+var credits_toggled = false
+
 
 func _process(delta: float):
 	# Get the internal VScrollBar and increment its value
-	var scrollbar = get_v_scroll_bar()
-	scrollbar.value += scroll_speed * delta
+	if credits_toggled: 
+		scrollbar.value += scroll_speed * delta
+
+
+func _on_roll_credits_body_entered(body: Node2D) -> void:
+	credits_toggled = true
